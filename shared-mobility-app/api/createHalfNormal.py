Fixed bug in distance calculation and continued with Grid.process_data(df)

shared-mobility-app/api/createHalfNormal.py
@@ -2,14 +2,35 @@ import numpy as np
 from math import sqrt, floor
 from scipy.stats import halfnorm
 
-def generate_cdfs(sigma, distance, max_distance, checkNorm=True):
+def get_distances(distance, max_distance):
+    """ Generate a sorted list of all possible distances given the size of each grid cell and
+        the maximum distance. This relies on the pythagorean theorem!
+        Also creates a dictionary that maps distance with triangle dimensions
+    """
+    # find the ratio of max_distance to distance
+    # the side lengths of all the triangles can not exceed that ratio
+    factor = floor(max_distance/distance)
+    dists = set([0, max_distance])
+    triangles = {0:(0,0)} # make the dict that maps dist to triangle dimensions
+    # create list of distances that are purely in vertical or horizontal direction
+    for i in range(1, factor+1):
+        dists.add(i*distance)
+        triangles[i*distance] = (i, 0)
+    # create the triangles and include distances that are less than max_distance
+    for i in range(1, factor+1):
+        for j in range(i, factor+1):
+            hypotenuse_dist = sqrt(i**2+j**2) * distance
+            if hypotenuse_dist < max_distance:
+                dists.add(hypotenuse_dist)
+                triangles[hypotenuse_dist] = (i, j)
+    return sorted(list(dists)), triangles
+
+def generate_cdfs(sigma, distance, max_distance, checkNorm=True, returnTriangles=False):
     """ Given sigma, create half normal distribution and discretize by outputting a dictionary
         where the key is the distance and the value is the cdf at that distance
     """
     # create array of distances
-    l1 = list(np.arange(1, floor(max_distance/distance)+1, 1) * distance)
-    l2 = list(np.arange(1, floor(max_distance/(distance*sqrt(2)))+1, 1) * (distance * sqrt(2)))
-    distances = sorted([0, max_distance] + l1 + l2)
+    distances, triangles = get_distances(distance, max_distance)
     # print(f"Distances are {distances}")
     # create half normal distribution
     rv = halfnorm(scale=sigma, loc=0)
@@ -24,7 +45,15 @@ def generate_cdfs(sigma, distance, max_distance, checkNorm=True):
     norm_cdfs = {k: v / prob_sum for k, v in cdfs.items()}
     if checkNorm:
         assert int(round(sum(norm_cdfs.values(), 0.0))) == 1
-    return norm_cdfs
+    if returnTriangles:
+        # combine triangles into norm_cdfs
+        ds = [norm_cdfs, triangles]
+        combined_dict = {}
+        for k in norm_cdfs.keys():
+            combined_dict[k] = tuple(d[k] for d in ds)
+        return combined_dict
+    else:
+        return norm_cdfs
 
 def get_p0(sigma, distance, max_distance):
     """ From sigma value make half normal distribution and calculate p0 value.
@@ -68,4 +97,4 @@ def create_distribution(p0, distance, max_distance):
     """
     # find sigma from p0 value
     sigma = search_sigma(p0, distance, max_distance)
-    return generate_cdfs(sigma, distance, max_distance)
+    return generate_cdfs(sigma, distance, max_distance, returnTriangles=True)